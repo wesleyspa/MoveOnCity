6082de9eaed2ca5579380db8d9ea1f91ab28ef3f
Wesley Alves <wesleyspa@yahoo.com.br>
2016-04-24 22:37:06 -0300

    Desenvolvimento da tela de Novo Atendimento Alinhamento da Timeline Novas opções no menu

---
+++ MoveOn_MVC/Controllers/AtendimentoController.cs
@@ -0,0 +1,25 @@
+﻿using MoveOn_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoveOn_MVC.Controllers
+{
+    public class AtendimentoController : Controller
+    {
+        // GET: Atendimento
+        public ActionResult NovoAtendimento()
+        {
+            return View();
+        }
+
+        //public void NovoAtendimento(Atendimento atendimento)
+        //{
+        //    //-- Implementar inserção do atendimento no banco 
+
+        //    RedirectToAction("Index", "Home");
+        //}
+    }
+}
